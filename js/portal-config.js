88ecb7cfff23c66e8195e1e750d893a6656c942b
DavyNova28 <dxgagnon@hotmail.com>
2026-09-14 13:09:34 -0400

    Update portal-config.js

--- js/portal-config.js
+++ js/portal-config.js
@@ -12,7 +12,7 @@
 window.PORTAL_CONFIG = {
   dashboards: {
     prod: "https://davynova28.github.io/mini-golf-signage/dashboard-v3.html?version=117.3",
-    dev: "https://davynova28.github.io/mini-golf-signage-dev/dashboard-v3.html?version=117.3"
+    dev: "https://davynova28.github.io/mini-golf-signage-dev/dashboard-v3.html?version=118"
   },
 
   quickLinks: [
